8acca1a4dcfae98eb30cd59653bb01eb2140ef4a
henry <henrydeguzman.java73@gmail.com>
2019-09-10 11:15:57 +0800

    assets images and fixed getfile. adding per type and baseurl

--- application/models/api/getfile.php
+++ application/models/api/getfile.php
@@ -11,18 +11,22 @@ class getfile extends _API {
     public function __construct() {}
     protected function name($path){
         //echo $path;exit();
+        $basepath="";$_ext='.jpeg';
+        isset($_GET['basepath'])?$basepath=$_GET['basepath']."/":$basepath=PATH_UPLOAD;
+        isset($_GET['ext'])?$_ext=$_GET['ext']:$_ext=".jpeg";
+
         $tmp_path=explode('/',$path);
         $filename=array_pop($tmp_path);
         $path=implode('/',$tmp_path);
 
         $tmp=explode('.', $filename);
         $ext= strtolower(end($tmp));
-        $filepath= PATH_UPLOAD.$path;
-        //echo $filepath;exit();
+        $filepath= $basepath.$path;
+        //echo $filepath.'/'.$filename;exit();
         if(file_exists($filepath)&& !empty($ext)){
             list($width, $height, $type) = getimagesize($filepath.'/'.$filename);
             if(IMAGETYPE_JPEG==$type||IMAGETYPE_PNG==$type||IMAGETYPE_GIF==$type){
-                return $this->getImage($filepath, $filename, $type);
+                return $this->getImage($filepath, $filename, $_ext);
             }else{
                 //return $this->getDocument($filename);
             }
@@ -31,15 +35,16 @@ class getfile extends _API {
             exit('File: '.$filepath.' not found!');
         }
     }
-    private function getImage($filepath,$filename,$type){
+    private function getImage($filepath,$filename,$_ext='.jpeg'){
         $size=isset($_GET['size'])?$_GET['size']:0;
         $thumbnail='';
         if(!empty($size)&&in_array($size,array("32","64","120"))) {
             $thumbnail="/thumbnails/".$size."x".$size;
             $filename= strtok($filename,'.');
-            $filename=$filename.'.jpeg';
+            $filename=$filename.$_ext;
         }
         $filepath=$filepath.$thumbnail."/".$filename;
+        //echo $filepath;exit();
         $img_type= array(IMAGETYPE_JPEG => 'image/jpg',IMAGETYPE_GIF => 'image/gif',IMAGETYPE_PNG => 'image/png');
         header('Pragma: public');
         header('Cache-Control: max-age=86400, public');
