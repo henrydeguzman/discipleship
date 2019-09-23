52da415d3979d67ce806e42fde4313851a3c4785
henry <henrydeguzman.java73@gmail.com>
2019-09-24 00:42:56 +0800

    class file naming correction

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Manila');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://rtudiscipleship.com';
+$config['base_url'] = 'http://rtudiscipleship.com';
 
 /*
 |--------------------------------------------------------------------------
