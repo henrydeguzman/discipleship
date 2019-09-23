412f2e5824973121641e6be46ccb599860aab989
henry <henrydeguzman.java73@gmail.com>
2019-09-23 18:01:13 +0800

    config dev

--- index.php
+++ index.php
@@ -79,6 +79,7 @@ switch (ENVIRONMENT)
 
 	case 'testing':
 	case 'production':
+
 		ini_set('display_errors', 1);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
 		{
@@ -88,6 +89,7 @@ switch (ENVIRONMENT)
 		{
 			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
 		}
+    error_reporting(-1);
 	break;
 
 	default:
